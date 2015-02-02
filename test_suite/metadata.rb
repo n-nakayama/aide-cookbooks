16e95b251e68bb8e2d6ff6f47a9eb1ee8be9b285
a.ide <fj0088iv@aa.jp.fujitsu.com>
2015-02-02 14:40:57 +0900

    2015/2/2

---
+++ test_suite/metadata.rb
@@ -0,0 +1,7 @@
+name "test_suite"
+description "runs cookbook tests for FJ"
+maintainer  "FJ NextS5 WLM OpsWorks"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'fj_s5_wlm_opsworks_commons'
